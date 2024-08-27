5bf6566b0175d02d4d0e9443363524c60efdafaf
MNavedQureshi <126236487+MNavedQureshi@users.noreply.github.com>
2024-08-27 14:20:10 +0400

    Infographics change
    smart table analyzer change

--- backend/danswer/tools/text_to_sql/sql_generation_tool.py
+++ backend/danswer/tools/text_to_sql/sql_generation_tool.py
@@ -198,6 +198,7 @@ class SqlGenerationTool(Tool):
                 sql_generation_tool_output = self.generate_sql_for_dataframe.generate_sql_query(schema=dataframe.dtypes,
                                                                                                 requirement=query)
                 filtered_df = self.execute_sql_on_dataframe(df=dataframe, sql_query=sql_generation_tool_output)
+                result_list = filtered_df.to_dict('records')
             else:
                 filtered_df = None
                 sql_generation_tool_output = None
